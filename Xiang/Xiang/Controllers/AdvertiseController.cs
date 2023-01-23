31ef8f3a6aa90f041d6e47cf85426ad4a0e1ed1f
重製 <hpu970390@gmail.com>
2023-01-23 23:55:09 +0800

    Create Controller

---
+++ Xiang/Xiang/Controllers/AdvertiseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Xiang.Controllers
+{
+    public class AdvertiseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Xiang/Xiang/Controllers/SupplierController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Xiang.Controllers
+{
+    public class SupplierController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
