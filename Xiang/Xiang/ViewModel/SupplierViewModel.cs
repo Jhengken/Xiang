c6e50ac5c6c013e7e19b8cb16c91a2f365e5ff4c
Elin <c2973457@gmail.com>
2023-01-31 01:33:14 +0800

    Merge branch 'feature/ProductController基本功能' into feature/Suppliers
    # Conflicts:
    #	Xiang/.vs/ProjectEvaluation/xiang.metadata.v5.1
    #	Xiang/.vs/ProjectEvaluation/xiang.projects.v5.1
    #	Xiang/.vs/Xiang/DesignTimeBuild/.dtbcache.v2
    #	Xiang/.vs/Xiang/config/applicationhost.config
    #	Xiang/.vs/Xiang/v17/.futdcache.v2
    #	Xiang/.vs/Xiang/v17/.suo
    #	Xiang/Xiang/Models/Supplier.cs
    #	Xiang/Xiang/Program.cs
    #	Xiang/Xiang/Xiang.csproj
    #	Xiang/Xiang/bin/Debug/net6.0/Xiang.dll
    #	Xiang/Xiang/bin/Debug/net6.0/Xiang.pdb
    #	Xiang/Xiang/bin/Debug/net6.0/Xiang.staticwebassets.runtime.json
    #	Xiang/Xiang/obj/Debug/net6.0/Xiang.GeneratedMSBuildEditorConfig.editorconfig
    #	Xiang/Xiang/obj/Debug/net6.0/Xiang.assets.cache
    #	Xiang/Xiang/obj/Debug/net6.0/Xiang.csproj.AssemblyReference.cache
    #	Xiang/Xiang/obj/Debug/net6.0/Xiang.csproj.CoreCompileInputs.cache
    #	Xiang/Xiang/obj/Debug/net6.0/Xiang.csproj.FileListAbsolute.txt
    #	Xiang/Xiang/obj/Debug/net6.0/Xiang.dll
    #	Xiang/Xiang/obj/Debug/net6.0/Xiang.genruntimeconfig.cache
    #	Xiang/Xiang/obj/Debug/net6.0/Xiang.pdb
    #	Xiang/Xiang/obj/Debug/net6.0/project.razor.vs.json
    #	Xiang/Xiang/obj/Debug/net6.0/ref/Xiang.dll
    #	Xiang/Xiang/obj/Debug/net6.0/refint/Xiang.dll
    #	Xiang/Xiang/obj/Debug/net6.0/staticwebassets.build.json
    #	Xiang/Xiang/obj/Debug/net6.0/staticwebassets.development.json
    #	Xiang/Xiang/obj/Xiang.csproj.nuget.dgspec.json
    #	Xiang/Xiang/obj/Xiang.csproj.nuget.g.props
    #	Xiang/Xiang/obj/project.assets.json
    #	Xiang/Xiang/obj/project.nuget.cache
    #	Xiang/Xiang/obj/staticwebassets.pack.sentinel

--- Xiang/Xiang/ViewModel/SupplierViewModel.cs
+++ Xiang/Xiang/ViewModel/SupplierViewModel.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
+using Xiang.Models;
 
-namespace Xiang.Models
+namespace Xiang.ViewModel
 {
-    public partial class Supplier
+    public class SupplierViewModel
     {
-        public Supplier()
+
+        public SupplierViewModel()
         {
             Aorders = new HashSet<Aorder>();
             Products = new HashSet<Product>();
@@ -14,13 +14,17 @@ namespace Xiang.Models
 
         public int SupplierId { get; set; }
 
+        [DisplayName("姓名")]
         public string? Name { get; set; }
         public string? Email { get; set; }
 
+        [DisplayName("電話")]
         public string? Phone { get; set; }
 
+        [DisplayName("密碼")]
         public string? Password { get; set; }
 
+        [DisplayName("地址")]
         public string? Address { get; set; }
         public int? CreditPoints { get; set; }
         public bool? BlackListed { get; set; }
